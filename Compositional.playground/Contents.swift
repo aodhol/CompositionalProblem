ef64f9da6758ced523b52777cc4675f0208b9a44
Aodh O Lionaird <aodhol@gmail.com>
2023-08-20 15:19:16 +0200

    Try out approach suggested on StackOverlflow with an inner UICollectionView using Flow Layout

--- Compositional.playground/Contents.swift
+++ Compositional.playground/Contents.swift
@@ -6,7 +6,9 @@ import PlaygroundSupport
 class ViewController: UIViewController {
 
     private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+    fileprivate let shelves = [easyListeningShelf, jazzShelf, rockShelf, popShelf, danceMusicShelf, gospelShelf, metalShelf]
     private lazy var dataSource = createDataSource()
+    private lazy var registration = createCellRegistration()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,53 +40,144 @@ extension ViewController {
 
     func createSection(sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection {
         let itemSize = NSCollectionLayoutSize(
-            widthDimension: .absolute(90),
+            widthDimension: .fractionalWidth(1.0),
             heightDimension: .estimated(44)
         )
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
         let groupSize = NSCollectionLayoutSize(
-            widthDimension: .fractionalWidth(90 / layoutEnvironment.container.effectiveContentSize.width),
+            widthDimension: .fractionalWidth(1.0),
             heightDimension: .estimated(44)
         )
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
         let section = NSCollectionLayoutSection(group: group)
         section.interGroupSpacing = 16.0
-        section.orthogonalScrollingBehavior = .continuous
         return section
     }
 
-    func createDataSource() -> UICollectionViewDiffableDataSource<Shelf, Album> {
-        let registration = createCellRegistration()
+    func createDataSource() -> UICollectionViewDiffableDataSource<Int, Shelf> {
         return UICollectionViewDiffableDataSource(
             collectionView: collectionView,
-            cellProvider: { (collectionView, indexPath, item) -> UICollectionViewCell? in
+            cellProvider: { [registration] (collectionView, indexPath, item) -> UICollectionViewCell? in
                 let cell = collectionView.dequeueConfiguredReusableCell(using: registration, for: indexPath, item: item)
-                cell.layer.borderColor = UIColor.red.cgColor
-                cell.layer.borderWidth = 1.0
                 return cell
             }
         )
     }
 
-    func createCellRegistration() -> UICollectionView.CellRegistration<UICollectionViewListCell, Album> {
-        return UICollectionView.CellRegistration<UICollectionViewListCell, Album> { (cell, indexPath, album) in
-            var content = UIListContentConfiguration.subtitleCell()
-            content.text = album.title
-            content.secondaryText = album.description
-            cell.contentConfiguration = content
+    func createCellRegistration() -> UICollectionView.CellRegistration<MyCell, Shelf> {
+        // Specifying MyCell and Shelf here are redundant but keeping it here so as to
+        // not refactor the example too much just to illustrate the proposed
+        // solution to the problem.
+        return UICollectionView.CellRegistration<MyCell, Shelf> { (cell, indexPath, shelf) in
+            let flowLayout = UICollectionViewFlowLayout()
+                flowLayout.scrollDirection = .horizontal
+                flowLayout.estimatedItemSize = CGSize(width: 90, height: 44) // Wouldn't it have made more sense to use .automatic?
+                cell.nestedCollectionView.collectionViewLayout = flowLayout
+                cell.nestedCollectionView.dataSource = self
+                cell.nestedCollectionView.delegate = self
+                cell.nestedCollectionView.reloadData()
         }
     }
 
     func loadData() {
-        let shelves = [easyListeningShelf, jazzShelf, rockShelf, popShelf, danceMusicShelf, gospelShelf, metalShelf]
-        var snapshot = NSDiffableDataSourceSnapshot<Shelf, Album>()
-        snapshot.appendSections(shelves)
-        shelves.forEach { shelf in
-            snapshot.appendItems(shelf.albums, toSection: shelf)
+        var snapshot = NSDiffableDataSourceSnapshot<Int, Shelf>()
+        snapshot.appendSections((0..<shelves.count).enumerated().map { $0.offset })
+        shelves.enumerated().forEach { indexAndShelf in
+            let index = indexAndShelf.offset
+            let shelf = indexAndShelf.element
+            snapshot.appendItems([shelf], toSection: index)
         }
         dataSource.apply(snapshot)
     }
 }
+
+// MARK: - <UICollectionViewDataSource> for inner cell -
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+
+}
+
+extension ViewController: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        shelves.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        collectionView.register(AlbumCell.self, forCellWithReuseIdentifier: String(describing: AlbumCell.self))
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: AlbumCell.self), for: indexPath) as! AlbumCell
+        let shelf = shelves[indexPath.section]
+        let album = shelf.albums[indexPath.item]
+        cell.titleLabel.text = album.title
+        return cell
+    }
+}
+
+// MARK: - Inner cell definition -
+
+// A Cell with a UICollectionView and Flow Layout (Stackoverflow proposed approach).
+class MyCell: UICollectionViewCell {
+    static let identifier = "MyCell"
+
+    let nestedCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        contentView.addSubview(nestedCollectionView)
+
+        NSLayoutConstraint.activate([
+            nestedCollectionView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            nestedCollectionView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            nestedCollectionView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            nestedCollectionView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// A cell with an album title.
+class AlbumCell: UICollectionViewCell {
+
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.setContentCompressionResistancePriority(.required, for: .vertical)
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+
+    private func setupView() {
+        contentView.addSubview(titleLabel)
+        self.setContentCompressionResistancePriority(.required, for: .vertical)
+        // Add constraints
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 0),
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 0),
+            titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 0)
+        ])
+    }
+}
+
 
 let viewController = ViewController()
 
